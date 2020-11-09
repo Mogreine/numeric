a3fb291da0f4616d385a8874826fe58bbc646408
Mogreine <north7311@gmail.com>
2020-11-09 21:53:05 +0300

    Added report

--- src/hw1/hw1.py
+++ src/hw1/hw1.py
@@ -12,11 +12,16 @@ def exp_true(x):
 
 def exp_teilor(x, float_type=64):
     fl = np.float64
-    k = 60
+    k = 160
     if float_type == 32:
         fl = np.float32
-        k = 30
-    exp_series = [fl(x ** i) / fl(fact(i)) for i in range(k)]
+        k = 50
+    exp_series = [fl(1)]
+    last = exp_series[-1]
+    for i in range(1, k + 1):
+        exp_series.append(last / fl(i) * fl(x))
+        last = exp_series[-1]
+    # exp_series = [fl(x ** i) / fl(fact(i)) for i in range(k)]
     exp_series.sort()
     res = fl(0)
     for i in exp_series:
@@ -31,10 +36,10 @@ def exp_teilor_vec(x_arr, float_type=64):
     return np.array(res)
 
 
-def plot(x, *errs):
+def plot(x32, x64, err32, err64):
     trace1 = go.Scatter(
-        x=x,
-        y=errs[0],
+        x=x32,
+        y=err32,
         mode='lines',
         marker={'color': 'rgba(0, 255, 0, 0.8)'},
         name='float32',
@@ -42,8 +47,8 @@ def plot(x, *errs):
         yaxis='y1'
     )
     trace2 = go.Scatter(
-        x=x,
-        y=errs[1],
+        x=x64,
+        y=err64,
         mode='lines',
         marker=dict(color='rgba(255, 0, 0, 0.8)'),
         name='float64',
@@ -55,17 +60,25 @@ def plot(x, *errs):
     layout = go.Layout(
         title='Зависимость относительной ошибки от x',
         xaxis=dict(
-            domain=[0, 0.45]
+            domain=[0, 0.45],
+            title='x'
         ),
         yaxis=dict(
-            domain=[0, 0.65]
+            domain=[0, 0.65],
+            showexponent='all',
+            exponentformat='e',
+            title='Относительная погрешность'
         ),
         xaxis2=dict(
-            domain=[0.55, 1]
+            domain=[0.55, 1],
+            title='x'
         ),
         yaxis2=dict(
             domain=[0, 0.65],
-            anchor='x2'
+            anchor='x2',
+            showexponent='all',
+            exponentformat='e',
+            title='Относительная погрешность'
         )
     )
 
@@ -74,12 +87,14 @@ def plot(x, *errs):
 
 
 if __name__ == "__main__":
-    x = np.linspace(0, 20, 500)
-    y_true = exp_true(x)
-    y_mine32 = exp_teilor_vec(x, 32)
-    y_mine64 = exp_teilor_vec(x, 64)
+    x32 = np.linspace(0, 20, 1000)
+    x64 = np.linspace(0, 80, 1000)
+    y_true32 = exp_true(x32)
+    y_true64 = exp_true(x64)
+    y_mine32 = exp_teilor_vec(x32, 32)
+    y_mine64 = exp_teilor_vec(x64, 64)
 
-    err32 = np.abs(y_mine32 - y_true) / y_true
-    err64 = np.abs(y_mine64 - y_true) / y_true
+    err32 = np.abs(y_mine32 - y_true32) / y_true32
+    err64 = np.abs(y_mine64 - y_true64) / y_true64
 
-    plot(x, err32, err64)
+    plot(x32, x64, err32, err64)
