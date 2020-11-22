caa56e1394e3354f2d4f35053c54f0290c3785df
Mogreine <north7311@gmail.com>
2020-11-22 16:49:00 +0300

    Added 2 hw

--- src/hw2/hw2.py
+++ src/hw2/hw2.py
@@ -1,4 +1,7 @@
 import numpy as np
+from timeit import default_timer as timer
+import plotly.graph_objs as go
+import plotly
 
 
 def numpy_solve(A, b):
@@ -31,23 +34,69 @@ def func(x):
     return -(np.exp(2) * x - x + np.exp(1 - x) - np.exp(x + 1)) / (1 - np.exp(2))
 
 
-def is_correct(x, u, tol=1e-5):
+def get_accuracy(x, u):
     u_true = func(x)
-    return np.allclose(u, u_true, atol=tol)
+    tol = 1e-1
+    while np.allclose(u, u_true, atol=tol) and tol > 1e-17:
+        tol /= 10
+    return tol * 10
+
+
+def generate_sys(n=1000):
+    h = 1 / (n + 1)
+    b = np.arange(1, n + 1) * h
+    A = np.diagflat([2 * h ** (-2) + 1 for i in range(1, n + 1)]) \
+        + np.diagflat([-h ** (-2) for i in range(1, n)], k=-1) \
+        + np.diagflat([-h ** (-2) for i in range(1, n)], k=1)
+    return A, b
+
+
+def test_time(method, l=10, r=1000, step=1):
+    sz = []
+    elapsed_time = []
+    for i in range(l, r + 1, step):
+        A, b = generate_sys(i)
+        start_time = timer()
+        method(A, b)
+        elapsed_time.append(timer() - start_time)
+        sz.append(i)
+    return sz, elapsed_time
+
+
+def plot(x, y, names):
+    traces = []
+    method_colors = {
+        'thomas': {'color': 'rgba(0, 255, 0, 0.8)'},
+        'gauss': {'color': 'rgba(255, 0, 0, 0.8)'}
+    }
+    for i in range(len(names)):
+        traces.append(
+            go.Scatter(
+                x=x[i],
+                y=y[i],
+                mode='lines+markers',
+                marker=method_colors[names[i]],
+                name=names[i]
+            )
+        )
+
+    layout = go.Layout(
+        title='Зависимость ремени работы от n',
+        xaxis={
+            'title': 'n'
+        },
+        yaxis={
+            'title': 'time, ms'
+        }
+    )
+
+    fig = go.Figure(data=traces, layout=layout)
+    plotly.offline.iplot(fig)
 
 
 if __name__ == "__main__":
-    n = int(1e3)
-    h = 1 / (n + 1)
-    b = np.arange(1, n + 1) * h
-    A = np.diagflat([2 * h ** (-2) + 1 for i in range(1, n + 1)])\
-        + np.diagflat([-h ** (-2) for i in range(1, n)], k=-1)\
-        + np.diagflat([-h ** (-2) for i in range(1, n)], k=1)
-
-    sol_true = func(b)
-    sol_np = numpy_solve(A, b)
-    sol_thomas = thomas_solve(A, b)
-
-    print(f'numpy solve correct: {is_correct(b, sol_np)}')
-    print(f'diff numpy: {np.linalg.norm(sol_true - sol_np)}')
-    print(f'diff thomas: {np.linalg.norm(sol_true - sol_thomas)}')
+    r = 1500
+    step = 10
+    x_g, y_g = test_time(numpy_solve, l=10, r=r, step=step)
+    x_t, y_t = test_time(thomas_solve, l=10, r=r, step=step)
+    plot([x_g, x_t], [y_g, y_t], names=['gauss', 'thomas'])
